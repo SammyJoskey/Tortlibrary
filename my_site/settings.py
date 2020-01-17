8475ea5d3547f02d5878d18751ea83210b166227
SammyJoskey <pokertort@gmail.com>
2020-01-17 15:08:56 +1000

    make it better

--- my_site/settings.py
+++ my_site/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8su&x*7$g*o7lpnezf6&ykpy*j0b&g@stjaea6$f2b33$cpfv9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
